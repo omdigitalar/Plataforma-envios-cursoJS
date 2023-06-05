77341cdb1bd8f3823574cf4eb2ed7edb7e048198
manuel oubina <omdigital.ar@gmail.com>
2023-06-05 11:32:23 -0300

    Camino a la tercera entrega

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const formulario = document.getElementById("formulario");
-const mensaje = document.getElementById("mensaje");
-const verPedidosBtn = document.getElementById("ver-pedidos");
-const pedidos = JSON.parse(localStorage.getItem("pedidos")) || [];
+const formulario = document.getElementById("formulario"); //Conectamos el JS con el ID formulario creado en el HTML.
+const mensaje = document.getElementById("mensaje"); ////Conectamos el JS con el ID div creado en el HTML.
+const verPedidosBtn = document.getElementById("ver-pedidos"); // Conectamos el js con el boton del html.
+const pedidos = JSON.parse(localStorage.getItem("pedidos")) || []; // Creamos el local storage para almacenar los pedidos.
 
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -10,7 +10,7 @@ formulario.addEventListener("submit", function (event) {
   const pesoPedido = parseInt(pesoPedidoInput.value);
 
   if (isNaN(pesoPedido)) {
-    mensaje.innerText = "Inserte un peso válido";
+    mensaje.innerText = "Inserte un peso válido"; // Si el peso no es un numero, devuelve "Inserte un peso válido"
   } else {
     const nombre = document.getElementById("nombre").value;
     const direccion = document.getElementById("direccion").value;
@@ -18,17 +18,17 @@ formulario.addEventListener("submit", function (event) {
     const dni = document.getElementById("dni").value;
     const montoPedido = parseInt(document.getElementById("monto").value);
 
-    const envioGratis = montoPedido >= 15000;
+    const envioGratis = montoPedido >= 15000; // Si el precio del pedido es mayor a 15.000 tienen envīo gratis.
 
     let montoEnvio;
     if (envioGratis) {
       montoEnvio = "Envío gratis";
     } else {
-      const encontrado = valorEnvios.find((item) => item.peso >= pesoPedido);
-      montoEnvio = encontrado ? encontrado.precio : "N/A";
+      const encontrado = valorEnvios.find((item) => item.peso >= pesoPedido); // Si es menor a 15.000 va hacia el array donde estan los precios por pesoPedido, para chequear el valor.
+      montoEnvio = encontrado ? encontrado.precio : "N/A"; // En caso de que no tenga precio, xq es envío gratis, aparecerá N/A en lugar de null.
     }
 
-    const pedido = {
+    const pedido = { // Creamos el objeto del pedido con los datos que contienen.
       nombre,
       direccion,
       codigoPostal,
@@ -38,19 +38,19 @@ formulario.addEventListener("submit", function (event) {
       montoEnvio,
     };
 
-    pedidos.push(pedido);
+    pedidos.push(pedido); // Realizamos un push del pedido realizado hacia un array vacío que contiene los pedidos y se almacena en el localstorage.
     localStorage.setItem("pedidos", JSON.stringify(pedidos));
 
-    mensaje.innerHTML = "Envío generado con éxito<br><button id='crear-otro-envio'>Crear otro envío</button>";
+    mensaje.innerHTML = "Envío generado con éxito<br><button id='crear-otro-envio'>Crear otro envío</button>"; //Muestra el evento "Pedido creado con exito" y habilita el botón para crear un nuevo envío.
 
-    document.getElementById("crear-otro-envio").addEventListener("click", function () {
+    document.getElementById("crear-otro-envio").addEventListener("click", function () { //Cuando apretan en "Crear otro envío" se limpia el formulario inicial.
       formulario.reset();
       mensaje.innerHTML = "";
     });
   }
 });
 
-verPedidosBtn.addEventListener("click", function () {
+verPedidosBtn.addEventListener("click", function () { // Si apretamos en el botón "Ver pedidos", muesta el contenido del array vacío que fuimos pusheando.
   const pedidosHTML = pedidos.map((pedido) => {
     const {
       nombre,
@@ -64,21 +64,21 @@ verPedidosBtn.addEventListener("click", function () {
 
     return `
       <li class="pedido-card">
-        <strong>Nombre:</strong> ${nombre}<br>
-        <strong>Dirección:</strong> ${direccion}<br>
-        <strong>Código Postal:</strong> ${codigoPostal}<br>
-        <strong>DNI:</strong> ${dni}<br>
-        <strong>Monto Pedido:</strong> $${montoPedido}<br>
-        <strong>Peso Pedido:</strong> ${pesoPedido ? `${pesoPedido}kg` : "N/A"}<br>
-        <strong>Monto Envío:</strong> ${montoEnvio}<br>
+        <strong>Nombre</strong> ${nombre}<br>
+        <strong>Dirección</strong> ${direccion}<br>
+        <strong>Código Postal</strong> ${codigoPostal}<br>
+        <strong>DNI</strong> ${dni}<br>
+        <strong>Monto Pedido</strong> $${montoPedido}<br>
+        <strong>Peso Pedido</strong> ${pesoPedido ? `${pesoPedido}kg` : "N/A"}<br>
+        <strong>Monto Envío</strong> ${montoEnvio}<br>
       </li>
     `;
-  });
+  }); //Nos devuelve unas cards, con los contenidos que va buscando a traves de los ${}.
 
   mensaje.innerHTML = `<ul>${pedidosHTML.join("")}</ul>`;
 });
 
-const valorEnvios = [
+const valorEnvios = [ //Array con los precios de los envíos segun los pesos de los paquetes.
   { peso: 1, precio: 1000 },
   { peso: 2, precio: 1300 },
   { peso: 5, precio: 1500 },
